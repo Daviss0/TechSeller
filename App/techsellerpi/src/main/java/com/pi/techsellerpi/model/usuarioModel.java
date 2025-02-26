e29d5104de42a59834ee2dce04e75deff1d4855e
luanderson amparo <luandersonamparo@gmail.com>
2025-02-26 04:26:44 -0300

    correção de classe

--- App/techsellerpi/src/main/java/com/pi/techsellerpi/model/usuarioModel.java
+++ App/techsellerpi/src/main/java/com/pi/techsellerpi/model/usuarioModel.java
@@ -1,4 +1,15 @@
 package com.pi.techsellerpi.model;
 
 public class usuarioModel {
+
+    //Declaração de variaveis
+    private String nome, cpf, email, grupo, senha, status;
+
+    //declarando um construto padrão
+   public  usuarioModel(){}
+
+    public void UsuarioServlet(String email, String senha){
+        this.email = email;
+        this.senha = senha;
+    }
 }
--- App/techsellerpi/src/main/java/com/pi/techsellerpi/servlet/UsuarioServlet.java
+++ App/techsellerpi/src/main/java/com/pi/techsellerpi/servlet/UsuarioServlet.java
@@ -4,14 +4,5 @@ import com.pi.techsellerpi.model.usuarioModel;
 
 public class UsuarioServlet {
 
-    //Declaração de variaveis
-    private String nome, cpf, email, grupo, senha, status;
 
-    //declarando um construto padrão
-    public UsuarioServlet(){}
-
-    public UsuarioServlet(String email, String senha){
-        this.email = email;
-        this.senha = senha;
-    }
 }
